613402b55ec136878c3e4766dfc5bf5791bed862
Saul Diez-Guerra <saul@diezguerra.com>
2019-04-09 13:01:41 -0400

    feat(tests): idiomatic test

--- python/tests.py
+++ python/tests.py
@@ -1,8 +1,9 @@
-import pytest
 from datetime import date
-from app import today
+from app import app
+
 
 def test_today():
-    body, status, _ = today()
-    assert status == 200
-    assert body == '{"today": "%s"}' % date.today()
+    with app.test_client() as cli:
+        resp = cli.get('/today')
+        assert resp.status_code == 200
+        assert resp.json == {"today": "{}".format(date.today())}
