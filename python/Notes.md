b6311ccfdde8bda8bf0a73b39477b026d0ca4a90
Timo Tyrrell <timomitchel23@gmail.com>
2019-04-12 11:34:17 -0600

    Add notes, endpoints for each day, and tests.

---
+++ python/Notes.md
@@ -0,0 +1,9 @@
+### Install/Setup
+
+- I had to run <code>pip3 install -r requirements.txt</code>
+- Then to run the tests I had to run <code>python3 -m pytest tests.py</code>
+- I wanted the verbose output so I rand <code>python3 -m pytest -v tests.py</code> while I wrote tests.
+
+### Development
+
+- I used this snippet <code>import code; code.interact(local=dict(globals(), **locals()))</code> To interact with data in the terminal.
--- python/app.py
+++ python/app.py
@@ -2,12 +2,44 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import date
 import requests
+import re
 
 
 app = Flask(__name__)
 CORS(app)
 
+URL = "https://www.thinkful.com/api/advisors/availability"
+
+r = requests.get(url = URL)
+data = r.json()
+this_weeks_dates = list(data.keys())
+sorted_dates = sorted(this_weeks_dates, key=lambda x: int(re.search(r'\d+$', x).group()))
 
 @app.route("/today", methods=["GET"])
 def today():
-    return jsonify({"today": date.today().isoformat()})
+    return jsonify({"today": sorted_dates[0]})
+
+@app.route("/tomorrow", methods=["GET"])
+def tomorrow():
+    return jsonify({"tomorrow": sorted_dates[1]})
+
+@app.route("/two_days_from_today", methods=["GET"])
+def two_days_from_today():
+    return jsonify({"two_days_from_today": sorted_dates[2]})
+
+@app.route("/three_days_from_today", methods=["GET"])
+def three_days_from_today():
+    return jsonify({"three_days_from_today": sorted_dates[3]})
+
+
+@app.route("/four_days_from_today", methods=["GET"])
+def four_days_from_today():
+    return jsonify({"four_days_from_today": sorted_dates[4]})
+
+@app.route("/five_days_from_today", methods=["GET"])
+def five_days_from_today():
+    return jsonify({"five_days_from_today": sorted_dates[5]})
+
+@app.route("/six_days_from_today", methods=["GET"])
+def six_days_from_today():
+    return jsonify({"six_days_from_today": sorted_dates[6]})
