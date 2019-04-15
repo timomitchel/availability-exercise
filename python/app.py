11316003546d682d22ef328418dce8ba49b93a3b
Timo Tyrrell <timomitchel23@gmail.com>
2019-04-15 09:41:57 -0600

    Update controller to only have one route - sort and group by instructor id - Test route.

--- python/app.py
+++ python/app.py
@@ -1,45 +1,30 @@
+import code
 from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import date
+
 import requests
 import re
-
 
 app = Flask(__name__)
 CORS(app)
 
-URL = "https://www.thinkful.com/api/advisors/availability"
+def make_request():
+    URL = "https://www.thinkful.com/api/advisors/availability"
+    return requests.get(url = URL)
 
-r = requests.get(url = URL)
-data = r.json()
+data = make_request().json()
 this_weeks_dates = list(data.keys())
 sorted_dates = sorted(this_weeks_dates, key=lambda x: int(re.search(r'\d+$', x).group()))
 
-@app.route("/today", methods=["GET"])
-def today():
-    return jsonify({"today": sorted_dates[0]})
-
-@app.route("/tomorrow", methods=["GET"])
-def tomorrow():
-    return jsonify({"tomorrow": sorted_dates[1]})
-
-@app.route("/two_days_from_today", methods=["GET"])
-def two_days_from_today():
-    return jsonify({"two_days_from_today": sorted_dates[2]})
-
-@app.route("/three_days_from_today", methods=["GET"])
-def three_days_from_today():
-    return jsonify({"three_days_from_today": sorted_dates[3]})
+def sort_by_id(sorted_dates):
+    sorted_by_id = {}
+    for val in sorted_dates:
+        for key, value in sorted(data[val].items()):
+            sorted_by_id.setdefault(value, []).append(key)
+    return sorted_by_id
 
 
-@app.route("/four_days_from_today", methods=["GET"])
-def four_days_from_today():
-    return jsonify({"four_days_from_today": sorted_dates[4]})
-
-@app.route("/five_days_from_today", methods=["GET"])
-def five_days_from_today():
-    return jsonify({"five_days_from_today": sorted_dates[5]})
-
-@app.route("/six_days_from_today", methods=["GET"])
-def six_days_from_today():
-    return jsonify({"six_days_from_today": sorted_dates[6]})
+@app.route("/available_times", methods=["GET"])
+def available_times():
+    return jsonify({"available_times": sort_by_id(sorted_dates)})
--- python/tests.py
+++ python/tests.py
@@ -2,68 +2,8 @@ import datetime
 from app import app
 
 
-def test_today():
+def test_available_times():
     with app.test_client() as cli:
-        resp = cli.get('/today')
+        resp = cli.get('/available_times')
         assert resp.status_code == 200
-        assert resp.json == {"today": "{}".format(datetime.date.today())}
-
-
-def test_tomorrow():
-    with app.test_client() as cli:
-        resp = cli.get('/tomorrow')
-        assert resp.status_code == 200
-        assert resp.json == {
-            "tomorrow":
-            "{}".format(datetime.date.today() + datetime.timedelta(days=1))
-        }
-
-
-def test_two_days_from_today():
-    with app.test_client() as cli:
-        resp = cli.get('/two_days_from_today')
-        assert resp.status_code == 200
-        assert resp.json == {
-            "two_days_from_today":
-            "{}".format(datetime.date.today() + datetime.timedelta(days=2))
-        }
-
-
-def test_three_days_from_today():
-    with app.test_client() as cli:
-        resp = cli.get('/three_days_from_today')
-        assert resp.status_code == 200
-        assert resp.json == {
-            "three_days_from_today":
-            "{}".format(datetime.date.today() + datetime.timedelta(days=3))
-        }
-
-
-def test_four_days_from_today():
-    with app.test_client() as cli:
-        resp = cli.get('/four_days_from_today')
-        assert resp.status_code == 200
-        assert resp.json == {
-            "four_days_from_today":
-            "{}".format(datetime.date.today() + datetime.timedelta(days=4))
-        }
-
-
-def test_five_days_from_today():
-    with app.test_client() as cli:
-        resp = cli.get('/five_days_from_today')
-        assert resp.status_code == 200
-        assert resp.json == {
-            "five_days_from_today":
-            "{}".format(datetime.date.today() + datetime.timedelta(days=5))
-        }
-
-
-def test_six_days_from_today():
-    with app.test_client() as cli:
-        resp = cli.get('/six_days_from_today')
-        assert resp.status_code == 200
-        assert resp.json == {
-            "six_days_from_today":
-            "{}".format(datetime.date.today() + datetime.timedelta(days=6))
-        }
+        assert resp.json['available_times']
